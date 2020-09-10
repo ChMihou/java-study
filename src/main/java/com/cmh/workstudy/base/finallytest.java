e3c4f0a9b7dd263afea7015b2d5b45ec866a775b
Admin <rambo@mobplus.net>
2020-09-10 15:04:08 +0800

    提交

---
+++ src/main/java/com/cmh/workstudy/base/finallytest.java
@@ -0,0 +1,21 @@
+package com.cmh.workstudy.base;
+
+import static java.lang.System.exit;
+
+public class finallytest{
+    public static int testFinally(){
+        try {
+
+            return 1;
+        } catch (Exception e) {
+            return 0;
+        }finally{
+            System.out.println("execute finally");
+            return 3;
+        }
+    }
+    public static void main(String[] args){
+        int result = testFinally();
+        System.out.println(result);
+    }
+}
