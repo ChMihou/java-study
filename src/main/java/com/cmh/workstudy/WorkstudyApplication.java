c3f26a8c44314a98dbdfaad15356d97d117e2493
Admin <rambo@mobplus.net>
2020-09-10 14:56:40 +0800

    提交

--- src/main/java/com/cmh/workstudy/WorkstudyApplication.java
+++ src/main/java/com/cmh/workstudy/WorkstudyApplication.java
@@ -1,9 +1,15 @@
 package com.cmh.workstudy;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
 
 @SpringBootApplication
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+@Import(FdfsClientConfig.class)
 public class WorkstudyApplication {
 
     public static void main(String[] args) {
