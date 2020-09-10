cf7b17a3531b65577a0d2cae85b20a51b90715c2
Admin <rambo@mobplus.net>
2020-09-10 15:04:25 +0800

    提交

---
+++ src/main/java/com/cmh/workstudy/utils/FastDFSClientWrapper.java
@@ -0,0 +1,29 @@
+package com.cmh.workstudy.utils;
+
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class FastDFSClientWrapper {
+    @Autowired
+    private FastFileStorageClient storageClient;
+
+    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        StorePath storePath = storageClient.uploadFile((InputStream)file.getInputStream(),file.getSize(), FilenameUtils.getExtension(file.getOriginalFilename()),null);
+        return getResAccessUrl(storePath);
+    }
+
+    // 封装文件完整URL地址
+    private String getResAccessUrl(StorePath storePath) {
+        String fileUrl = "http://192.168.32.128:22122" + "/" + storePath.getFullPath();
+        return fileUrl;
+    }
+}
