8fdb75e9c43798072d097e2e8673cd8141ce90f9
Admin <rambo@mobplus.net>
2020-09-11 15:07:10 +0800

    bindresult验证数据的可靠性

---
+++ src/main/java/com/cmh/workstudy/Controller/TestController.java
@@ -0,0 +1,46 @@
+package com.cmh.workstudy.Controller;
+
+import com.cmh.workstudy.model.Parameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+public class TestController {
+
+    private static Logger log = LoggerFactory.getLogger(TestController .class);
+
+    @PostMapping(value = "param")
+    public @ResponseBody ResponseEntity<Parameter> testBindingResult(@Valid Parameter parameter,
+                                                                        BindingResult bindingResult) {
+        log.info("test start");
+        if(bindingResult.hasErrors()){
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            fieldErrors.forEach(fieldError -> {
+                //日志打印不符合校验的字段名和错误提示
+                System.out.println("error field is : {} ,message is : {}"+ fieldError.getField()+fieldError.getDefaultMessage());
+            });
+            for(int i=0;i<fieldErrors.size();i++){
+                //控制台打印不符合校验的字段名和错误提示
+                System.out.println("error field is :"+fieldErrors.get(i).getField()+",message is :"+fieldErrors.get(i).getDefaultMessage());
+            }
+            // pesponsibles.setError_msg(fieldErrors);
+            return new ResponseEntity<>(parameter, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(parameter, HttpStatus.OK);
+    }
+
+    @RequestMapping("paramer")
+    public String param(){
+        return "paramer";
+    }
+}
---
+++ src/main/java/com/cmh/workstudy/model/Parameter.java
@@ -0,0 +1,70 @@
+package com.cmh.workstudy.model;
+
+import java.util.List;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+public class Parameter {
+
+    @NotNull(message="姓名不能为空")
+    private String name;
+
+    @Min(value = 18, message = "年龄必须大于18岁")
+    private int age;
+
+    @NotNull(message="hobbies不能为空")
+    private List<String> hobbies;
+
+    @NotNull(message="账号不能为空")
+    private String account;
+
+    @Size(min=5,max=10,message="密码的长度应该在5和10之间")
+    private String password;
+
+    @Email(message="邮箱格式错误")
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+    public void setHobbies(List<String> hobbies) {
+        this.hobbies = hobbies;
+    }
+    public String getAccount() {
+        return account;
+    }
+    public void setAccount(String account) {
+        this.account = account;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+}
